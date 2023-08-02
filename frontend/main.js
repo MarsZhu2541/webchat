e13ee0f92d99620d063b3af5ce0380425d129861
Mars Zhu <mars.zhu@valtech.com>
2023-08-02 17:28:31 +0800

    add user icon

--- frontend/main.js
+++ frontend/main.js
@@ -76,6 +76,7 @@ const App = {
                 if (this.user.id !== -1) {
                     this.initWebSocket();
                 }
+                resolve()
             })
         },
         initWebSocket() {
