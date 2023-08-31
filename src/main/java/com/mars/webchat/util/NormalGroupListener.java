af944a91762f92ea4028597fac55895ae1c7631c
Mars Zhu <mars.zhu@valtech.com>
2023-08-31 16:38:38 +0800

    add qq bot

--- src/main/java/com/mars/webchat/util/NormalGroupListener.java
+++ src/main/java/com/mars/webchat/util/NormalGroupListener.java
@@ -7,12 +7,17 @@ import net.itbaima.robot.event.RobotListenerHandler;
 import net.itbaima.robot.listener.MessageListener;
 import net.itbaima.robot.service.RobotService;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.message.data.At;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @Slf4j
 @RobotListener
 public class NormalGroupListener extends MessageListener {
+
+    @Value("${itbaima.robot.username}")
+    private long qqNumber;
 
     @Autowired
     private ChatGPTServiceImpl chatGPTServiceImpl;
@@ -23,12 +28,20 @@ public class NormalGroupListener extends MessageListener {
     @RobotListenerHandler
     public void handleMessage(GroupMessageEvent event) {
         String message = event.getMessage().contentToString();
+
+        if (!(event.getMessage().get(1) instanceof At)) {
+            return;
+        }
+        if (((At) event.getMessage().get(1)).getTarget() != qqNumber) {
+            return;
+        }
         log.info("Received group message: {}", message);
         try {
             String chat = chatGPTServiceImpl.chat(message);
             log.info("Sent group message: {}", chat);
             robotService.sendMessageToGroup(event.getGroup().getId(), chat);
-        }catch (Exception e){
+        } catch (Exception e) {
+            log.error("Error when send message: ");
             robotService.sendMessageToGroup(event.getGroup().getId(), "出错了，请联系管理员qq2541884980");
         }
     }
