285fb7c8a5e4b3b7ba6e55001ef02538741cb25d
Mars Zhu <mars.zhu@valtech.com>
2023-12-04 17:09:17 +0800

    upgrade dependency

--- src/test/java/com/mars/webchat/ChatGPTTest.java
+++ src/test/java/com/mars/webchat/ChatGPTTest.java
@@ -1,18 +1,22 @@
 package com.mars.webchat;
 
 import com.mars.webchat.service.ChatGPTService;
+import com.mars.webchat.service.impl.ChatGPTServiceImpl;
+import com.plexpt.chatgpt.entity.chat.Message;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import java.util.List;
+
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ChatGPTTest {
 
-    @Autowired
-    private ChatGPTService chatGPTServiceImpl;
+
+    private ChatGPTServiceImpl chatGPTServiceImpl = new ChatGPTServiceImpl();
 
     @Test
     void test() {
-//        System.out.println(chatGPTServiceImpl.chat("你是谁"));
+        System.out.println(chatGPTServiceImpl.chat(List.of(Message.of("你是谁"))));
     }
 }
